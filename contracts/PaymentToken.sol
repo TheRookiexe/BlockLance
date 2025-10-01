1d72f15acb76e671d43f6b3a74c2c7a958bb8bc1
TheRookiexe <mishrxa36@gmail.com>
2025-10-01 19:08:06 +0530

    hardhat setup complete and also added token contract.

---
+++ contracts/PaymentToken.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract PaymentToken is ERC20 {
+    constructor(uint256 initialsupply) ERC20("FreelanceToken", "FLT") {
+        _mint(msg.sender, initialSupply);
+    }
+}
