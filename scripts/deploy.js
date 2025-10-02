937ee1ed2fc55b1a15362e806a90532d54098857
TheRookiexe <mishrxa36@gmail.com>
2025-10-02 22:33:47 +0530

    tested the contract workflow.

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,21 +1,20 @@
-import { ethers } from "hardhat";
+const { ethers } = require("hardhat");
 
 async function main() {
-  const [deployer] = await ethers.getSigners();
-  console.log("Deploying contracts with:", deployer.address);
+    const [deployer] = await ethers.getSigners();
 
-  const Token = await ethers.getContractFactory("PaymentToken");
-  const token = await Token.deploy(1000000n); 
-  await token.deployed();
-  console.log("PaymentToken deployed at:", token.address);
+    const Token = await ethers.getContractFactory("PaymentToken");
+    const token = await Token.deploy(ethers.utils.parseUnits("100000", 18));
+    await token.deployed();
+    console.log("PaymentToken deployed at:", token.address);
 
-  const Marketplace = await ethers.getContractFactory("FreelanceMarketplace");
-  const marketplace = await Marketplace.deploy(token.address);
-  await marketplace.deployed();
-  console.log("FreelanceMarketplace deployed at:", marketplace.address);
+    const Marketplace = await ethers.getContractFactory("FreelanceMarketplace");
+    const marketplace = await Marketplace.deploy(token.address);
+    await marketplace.deployed();
+    console.log("Marketplace deployed at:", marketplace.address);
 }
 
 main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
+    console.error(error);
+    process.exitCode = 1;
 });
